88cb539e91742150dfe9901543a4c6d4ba96d32b
AJF <AJF@Javier>
2015-09-13 22:52:02 -0600

    login, registro

---
+++ proyecto1s22015_201212840/src/java/java/LoginServlet.java
@@ -0,0 +1,60 @@
+package com.edu4java.servlets;
+//package java;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+import servicios.AdministradorService;
+
+public class LoginServlet extends HttpServlet {
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/192.168.0.116_8080/Servidor/administradorService.wsdl")
+    private AdministradorService service;
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String user = req.getParameter("correo");
+		String pass = req.getParameter("contrasena");
+		if (ingreso(user,pass)) {
+			response(resp, "login ok");
+		} else {
+			response(resp, "invalid login");
+		}
+               // resp.sendRedirect("login.html");
+            /*if(req.getParameter("button1") != null)
+            {
+                otro(resp);
+            }*/
+	}
+
+	private void response(HttpServletResponse resp, String msg)
+			throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<t1>" + msg + "</t1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+    private boolean ingreso(java.lang.String arg0, java.lang.String arg1) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        
+        servicios.Administrador port = service.getAdministradorPort();
+        return port.ingreso(arg0, arg1);
+    }
+    
+    public void otro(HttpServletResponse resp)
+			throws IOException {
+        PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<t1>Entro al metodo"+ "</t1>");
+		out.println("</body>");
+		out.println("</html>");
+    }
+}
